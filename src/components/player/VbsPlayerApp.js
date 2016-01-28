6adcf27c44b39e7518fecc620678cbea53e1a183
pronin <denis.pronin@sibers.com>
2016-01-28 15:03:25 +0600

    [V2-591] - keyword layout

--- src/components/player/VbsPlayerApp.js
+++ src/components/player/VbsPlayerApp.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react'
 import Player from './Player';
+import { Tabs, Tab } from 'react-bootstrap';
+import KeywordsTabContent from './KeywordsTabContent'
 
 export class VbsPlayerApp extends React.Component {
   static propTypes = {
@@ -22,6 +24,17 @@ export class VbsPlayerApp extends React.Component {
                 playerType="JwPlayer"
                 playerState={this.props.playerState.players[this.props.mediaId] || {loading: true}}
                 actions={this.props.actions} />
+
+        <Tabs className="listing__tabs">
+          <Tab eventKey={1} title="Keywords">
+            <KeywordsTabContent mediaId={this.props.mediaId}
+                                mediaState={this.props.mediaState}
+                                actions={this.props.actions} />
+          </Tab>
+          <Tab eventKey={2} title="Detection">Tab 2 content</Tab>
+          <Tab eventKey={3} title="Prediction">Tab 3 content</Tab>
+        </Tabs>
+
       </div>
     )
   }
