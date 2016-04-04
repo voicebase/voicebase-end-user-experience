ee7f3295441c1d60c3dd027fff98d36be4339dbf
pronin <denis.pronin@sibers.com>
2016-04-04 15:44:20 +0600

    optimization of keywords

--- src/components/player/Keywords.js
+++ src/components/player/Keywords.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import { COLORS } from '../../common/Common'
 import KeywordTopic from './KeywordTopic'
+import KeywordWord from './KeywordWord'
 
 export class Keywords extends React.Component {
   static propTypes = {
@@ -18,26 +18,8 @@ export class Keywords extends React.Component {
     this.props.actions.setActiveTopic(this.props.mediaId, topicId, this.props.type);
   }
 
-  setMarkers(keyword, activeSpeakerId) {
-    let color = COLORS[0];
-    let times = (activeSpeakerId) ? keyword.t[activeSpeakerId] : this.getAllKeywordTimes(keyword);
-    let markers = times.map(_time => {
-      let time = parseFloat(_time);
-      return {
-        time,
-        keywordName: keyword.name,
-        color
-      }
-    });
+  setMarkers(markers) {
     this.props.actions.setMarkers(this.props.mediaId, markers);
-  }
-
-  getAllKeywordTimes(keyword) {
-    let times = [];
-    Object.keys(keyword.t).forEach(speakerName => {
-      times = times.concat(keyword.t[speakerName]);
-    });
-    return times;
   }
 
   render () {
@@ -69,17 +51,12 @@ export class Keywords extends React.Component {
               {
                 activeTopic.keywordsIds.map(keywordId => {
                   let keyword = activeTopic.keywords[keywordId];
-                  let times = (activeSpeakerId) ? keyword.t[activeSpeakerId] : this.getAllKeywordTimes(keyword);
-                  if (!times) { // no times - no keyword.
-                    return null;
-                  }
                   return (
-                    <li key={'keyword-' + keywordId}>
-                      <a href="javascript:void(0)" onClick={this.setMarkers.bind(this, keyword, activeSpeakerId)}>
-                        <span className="listing__keywords__keyword-name">{keyword.name}</span>
-                        <span> ({times.length})</span>
-                      </a>
-                    </li>
+                    <KeywordWord key={'keyword-' + keywordId}
+                                 keyword={keyword}
+                                 activeSpeakerId={activeSpeakerId}
+                                 setMarkers={this.setMarkers.bind(this)}
+                    />
                   )
                 })
               }
